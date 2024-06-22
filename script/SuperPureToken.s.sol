9043afb4be74d4732321b9507df507eb734d29c1
hiromaru05s <maruru836818@gmail.com>
2024-06-22 01:51:01 -0400

    implemented super pure token contract with deploy script

---
+++ script/SuperPureToken.s.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.23;
+
+import { Script } from "lib/forge-std/src/Script.sol";
+import { SuperPureToken } from "../src/SuperPureToken.sol";
+
+contract DeployPureSuperToken is Script {
+    function run() external {
+        uint256 deployerPrivateKey = vm.envUint("DEPLOYER_PRIVATE_KEY");
+        vm.startBroadcast(deployerPrivateKey);
+
+        SuperPureToken token = new SuperPureToken();
+        token.initialize("Super Pure Token", "SPT", 1000 ether);
+
+        vm.stopBroadcast();
+    }
+}

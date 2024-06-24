80d9382539c9444ad047fd9f708092a028ea3858
shuhei tanaka <shuhei46491@gmail.com>
2024-06-25 04:37:10 +0900

    deploy script

--- script/PureSuperToken.s.sol
+++ script/PureSuperToken.s.sol
@@ -2,15 +2,17 @@
 pragma solidity 0.8.23;
 
 import {Script} from "lib/forge-std/src/Script.sol";
-import {SuperPureToken} from "../src/SuperPureToken.sol";
+import {PureSuperToken} from "../src/PureSuperToken.sol";
 
 contract DeployPureSuperToken is Script {
     function run() external {
         uint256 deployerPrivateKey = vm.envUint("DEPLOYER_PRIVATE_KEY");
         vm.startBroadcast(deployerPrivateKey);
 
-        SuperPureToken token = new SuperPureToken();
-        token.initialize("Super Pure Token", "SPT", 1000 ether);
+        address factory = 0xfcF0489488397332579f35b0F711BE570Da0E8f5;
+
+        PureSuperToken token = new PureSuperToken();
+        token.initialize(factory, "Super Pure Token", "SPT", 0xc3593524E2744E547f013E17E6b0776Bc27Fc614, 1000 ether);
 
         vm.stopBroadcast();
     }
